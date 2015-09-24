604ac6c20639b51827cfbcc9dcdb87cbe7d13f68
uchin <aswaja56@gmail.com>
2015-09-25 01:54:05 +0700

    Create Other Activity

--- app/src/main/java/com/muklasin/androiddeveloper/DisplayMessageActivity.java
+++ app/src/main/java/com/muklasin/androiddeveloper/DisplayMessageActivity.java
@@ -1,31 +1,28 @@
 package com.muklasin.androiddeveloper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-/** Called when the user clicks the Send button */
-public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        }
-
-public class MyActivity extends AppCompatActivity {
+public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
     @Override
