02da196f3d01e3c0dc27fb6acd7a463a8e02ac50
uchin <aswaja56@gmail.com>
2015-09-28 00:09:04 +0700

    Respond to Action Buttons and change to dark actionbar

--- app/src/main/java/com/muklasin/androiddeveloper/MyActivity.java
+++ app/src/main/java/com/muklasin/androiddeveloper/MyActivity.java
@@ -2,6 +2,7 @@ package com.muklasin.androiddeveloper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -39,17 +40,28 @@ public class MyActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void openSettings() {
+
+    }
+
+    private void openSearch() {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setMessage("Button search presed");
+        alertDialog.show();
+    }
+
 
 }
